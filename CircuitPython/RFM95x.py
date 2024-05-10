583f1010f8d7ced2a8842a8a977ac326f0813718
SP1RYTUSzz <157947055+SP1RYTUSzz@users.noreply.github.com>
2024-05-10 09:03:17 -0700

    Update RFM95x.py

--- CircuitPython/RFM95x.py
+++ CircuitPython/RFM95x.py
@@ -17,8 +17,8 @@ RADIO_FREQ_MHZ = 915.0  # Frequency of the radio in Mhz. Must match your
 
 # Define pins connected to the chip, use these if wiring up the breakout according to the guide:
 #These are for RFM9x, not SPI
-CS = digitalio.DigitalInOut(board.D5)
-RESET = digitalio.DigitalInOut(board.D6)
+CS = digitalio.DigitalInOut(board.D10)
+RESET = digitalio.DigitalInOut(board.D11)
 
 
 # Define the onboard LED
@@ -27,7 +27,7 @@ led.direction = digitalio.Direction.OUTPUT
 
 #init SPI
 with busio.SPI(SCK, MOSI, MISO) as spi_bus:
-    cs = digitalio.DigitalInOut(D257)
+    cs = digitalio.DigitalInOut(D25)
     device = SPIDevice(spi_bus, cs)
 # Initialize SPI bus.y
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
@@ -46,19 +46,18 @@ rfm9x.tx_power = 23
 # This is a limitation of the radio packet size, so if you need to send larger
 # amounts of data you will need to break it into smaller send calls.  Each send
 # call will wait for the previous one to finish before continuing.
-rx_raw_string = ""
-state = 0
+
 
 #Starting Tri's code
 #allocate memory
-txString0 = "This is UART0 channel\n"
-txString1 = "Speaking to UART1\n"
-rxString0 = ""
-rxString1 = ""
-txData0 = bytearray(1)
-rxData0 = bytearray(1)
-txData1 = bytearray(1)
-rxData1 = bytearray(1)
+tx_string0 = "This is from UART0 channel\n"
+tx_string1 = "Speaking to UART1\n"
+rx_string0 = ""
+rx_string1 = ""
+tx_data0 = bytearray(1)
+rx_data0 = bytearray(1)
+tx_data1 = bytearray(1)
+rx_data1 = bytearray(1)
 selection = bytearray(1)    #see which UART Pico is receiving and relaying from
 i0 = 0       #index for time message repeated
 i1 = 0
@@ -81,58 +80,61 @@ while True:
         pass
     try:
         spi.configure(baudrate=1000000)
-        cs.value = False
-        cs.value = True
     finally:
         spi.unlock()
-
-    #Starting Tri's Code
-    #Keep putting in new message after txString run out
-    if (txString0 == ""):
-        txString0 = "uart0 repeat time " + str(i0) + "\n"
-        i0 += 1
-    if (txString1 == ""):
-        txString1 = "uart1 repeating time " + str(i1) + "\n"
-        i1 += 1
-    #SPI ENCODE: split txString into 1 byte chunk
-    if (txString0 != ""):
-        txData0 = bytearray(txString0[0:1], "utf-8", "ignore")
-        txString0 = txString0[1:len(txString0)]            #len(txString)-1 if sht happened
-    if (txString1 != ""):
-        txData1 = bytearray(txString1[0:1], "utf-8", "ignore")
-        txString1 = txString1[1:len(txString1)]            #len(txString)-1 if sht happened
-    #SPI EXCHANGE: conversation, 1 byte per cs cycle
-    while not spi.try_lock():
-        pass
+        
     try:
-        spi.configure(baudrate=1000000)
-        cs.value = False
-        spi.write_readinto('#', selection)
-        cs.value = True
-    finally:
-        spi.unlock()
+        # Keep putting in new message after tx_string run out
+        if (tx_string0 == ""):
+            tx_string0 = "uart0 repeat time " + str(i0) + "\n"
+            i0 += 1
+        if (tx_string1 == ""):
+            tx_string1 = "uart1 repeating time " + str(i1) + "\n"
+            i1 += 1
+        
+        # SPI ENCODE: split tx_string into 1 byte chunk
+        if tx_string0:
+            tx_data0 = bytearray(tx_string0[0:1], "utf-8", "ignore")
+            tx_string0 = tx_string0[1:]  # len(tx_string)-1 if sht happened
+        if tx_string1:
+            tx_data1 = bytearray(tx_string1[0:1], "utf-8", "ignore")
+            tx_string1 = tx_string1[1:]  # len(tx_string)-1 if sht happened
 
-    if (selection == '`'):
+        
         while not spi.try_lock():
             pass
         try:
             spi.configure(baudrate=1000000)
+            # SPI EXCHANGE: conversation, 1 byte per cs cycle
             cs.value = False
-            spi.write_readinto(txData0, rxData0)
+            spi.write_readinto(b'#', selection)
             cs.value = True
-            cs.value = False
-            spi.write_readinto(txData1, rxData1)
-            cs.value = True
+            if (True):			#selection == b'`':
+                cs.value = False
+                spi.write_readinto(tx_data0, rx_data0)
+                cs.value = True
+                print(tx_data0, rx_data0)
+                cs.value = False
+                spi.write_readinto(tx_data1, rx_data1)
+                cs.value = True
+                rx_string0 += rx_data0.decode('utf-8', 'ignore')
+                if rx_data0.decode('utf-8', 'ignore') == '\n':
+                    rfm9x.send(bytes("Message from UART0: ", rx_string0))
+                    print("Message from UART0: ", rx_string0)
+                    rx_string0 = ""
+                rx_string1 += rx_data1.decode('utf-8', 'ignore')
+                if rx_data1.decode('utf-8', 'ignore') == '\n':
+                    rfm9x.send(bytes("Message from UART1: ", rx_string1))
+                    print("Message from UART1: ", rx_string1)
+                    rx_string1 = ""
+
+                # print(rx_data0, rx_data1)
         finally:
             spi.unlock()
-            
-        rxString0 += rxData0.decode('utf-8', 'replace')
-        if (rxData0.decode('utf-8', 'ignore') == '\n'):
-            rfm9x.send(bytes("Message from UART0: ", rxString0))
-            rxString0 = ""
-        rxString1 += rxData1.decode('utf-8', 'replace')
-        if (rxData1.decode('utf-8', 'ignore') == '\n'):
-            rfm9x.send(bytes("Message from UART1: ", rxString1))
-            rxString1 = ""
-            
-    time.sleep(0.01)
+
+        time.sleep(0.01)
+    except UnicodeError:
+        print("Unicode Error time ", unicode_error_occurence)
+        unicode_error_occurence += 1
+
+
