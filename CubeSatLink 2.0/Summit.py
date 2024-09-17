fdc7a2f0a35f8bebd4451cd30b1886f38caf1912
SP1RYTUSzz <157947055+SP1RYTUSzz@users.noreply.github.com>
2024-09-17 15:36:50 -0700

    Update Summit.py

--- CubeSatLink 2.0/Summit.py
+++ CubeSatLink 2.0/Summit.py
@@ -1,52 +1,56 @@
-# BLV Summit Commit 9/10
-#
+# BLV Summit Flight Transceiver
 import time
 import board
 import busio
 import digitalio
 import adafruit_rfm9x
 
-# Define radio parameters.
-RADIO_FREQ_MHZ = 915.0
 
+# Initialize UART bus
+uart = busio.UART(board.A2, board.A3, baudrate=9600, bits = 8, parity = None, timeout = 10)
+
+# Initialize SPI bus & RFM Radio
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 CS = digitalio.DigitalInOut(board.D10)
 RESET = digitalio.DigitalInOut(board.D11)
-
-# Initialize SPI bus.
-spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-# Initialze RFM radio
+RADIO_FREQ_MHZ = 927.0
 rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
 
-# set delay before transmitting ACK (seconds)
+# rfm9x settings
 rfm9x.ack_delay = 0.1
-# set node addresses
 rfm9x.node = 2
 rfm9x.destination = 1
-# initialize counter
 counter = 0
 ack_failed_counter = 0
 
+rfm9x.tx_power = 23
+#rfm9x.signal_bandwidth = 62500
+#rfm9x.coding_rate = 6
+rfm9x.spreading_factor = 7
+rfm9x.enable_crc = True
+
 transmit_interval = 1
+
+
 # Wait to receive packets.
 print("Waiting for packets...")
 time_now = time.monotonic()
 while True:
-    # Look for a new packet: only accept if addresses to my_node
+    
+    
     packet = rfm9x.receive(with_ack=True, with_header=True)
-    # If no packet was received during the timeout then None is returned.
+    # print after any packet received
     if packet is not None:
-        # Received a packet!
-        # Print out the raw bytes of the packet:
         print("Received (raw header):", [hex(x) for x in packet[0:4]])
         print("Received (raw payload): {0}".format(packet[4:]))
         print("RSSI: {0}".format(rfm9x.last_rssi))
         print("SNR: {0}".format(rfm9x.last_snr))
-        # send reading after any packet received
+        
+    # send a mesage to destination_node from my_node
     if time.monotonic() - time_now > transmit_interval:
-        # reset timeer
         time_now = time.monotonic()
         counter += 1
-        # send a  mesage to destination_node from my_node
+        # error
         if not rfm9x.send_with_ack(
             bytes("message from Summit. (Node {}, count# {})".format(rfm9x.node, counter), "UTF-8")
         ):
