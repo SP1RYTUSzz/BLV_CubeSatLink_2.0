98ef11f1f04d0acf3e21fe2a409d683386adb667
SP1RYTUSzz <157947055+SP1RYTUSzz@users.noreply.github.com>
2025-02-05 23:55:29 -0800

    Backup BaseStation.py after parking lot test

--- CubeSatLink 2.0/BaseStation.py
+++ CubeSatLink 2.0/BaseStation.py
@@ -1,1 +1,63 @@
+# CubeSatLink GroundStation Node
+# Connect to antenna (3dB or Yagi) before plug any power in)
+# Author: Tri Do
+#
+import time
+import board
+import busio
+import digitalio
+import adafruit_rfm9x
+
+# Define radio parameters.
+RADIO_FREQ_MHZ = 902.0  # Frequency of the radio in Mhz. Must match your
+# module! Can be a value like 915.0, 433.0, etc.
+
+# Define pins connected to the chip.
+# set GPIO pins as necessary - this example is for Raspberry Pi
+CS = digitalio.DigitalInOut(board.D10)
+RESET = digitalio.DigitalInOut(board.D11)
+
+# Initialize SPI bus.
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+# Initialze RFM radio
+rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ, agc = True)
+
+# enable CRC checking
+rfm9x.enable_crc = True
+# set delay before transmitting ACK (seconds)
+rfm9x.ack_delay = 0.1
+# set node addresses
+rfm9x.node = 2
+rfm9x.destination = 1
+# initialize counter
+counter = 0
+ack_failed_counter = 0
+
+# rfm setting
+rfm9x.tx_power = 23
+rfm9x.coding_rate = 8
+#rfm9x.signal_bandwidth = 7800
+rfm9x.spreading_factor = 9		#higher = lower bitrate
+
+# Wait to receive packets.
+print("Waiting for packets...")
+while True:
+    # Look for a new packet: only accept if addresses to my_node
+    packet = rfm9x.receive(with_ack=True, with_header=True)
+    # If no packet was received during the timeout then None is returned.
+    if packet is not None:
+        # Received a packet!
+        # Print out the raw bytes of the packet:
+        print("Received (raw header):", [hex(x) for x in packet[0:4]])
+        print("Received (raw payload): {0}".format(packet[4:]))
+        print("RSSI: {0}, SNR: {1}".format(rfm9x.last_rssi, rfm9x.last_snr))
+        # send response 2 sec after any packet received
+        time.sleep(2)
+        counter += 1
+        # send a  mesage to destination_node from my_node
+        if not rfm9x.send_with_ack(
+            bytes("response from node {} {}".format(rfm9x.node, counter), "UTF-8")
+        ):
+            ack_failed_counter += 1
+            print(" No Ack: ", counter, ack_failed_counter)
 
