7bbfb8823f134ab4a7bb95ab3707ff335e8c52da
SP1RYTUSzz <157947055+SP1RYTUSzz@users.noreply.github.com>
2025-02-21 12:40:37 -0800

    Update Summit.py

--- CircuitPython/Summit.py
+++ CircuitPython/Summit.py
@@ -1,164 +1,100 @@
-# BLV Flight Transceiver (Summit).
+# CubeSatLink Flight Transceiver Node (Summit)
+# Connect to antenna before plug any power in
+# Author: Tri Do
 
+import time
 import board
 import busio
 import digitalio
-from adafruit_datetime import datetime
-import time
 import adafruit_rfm9x
-import os    # To utilize file operations
 
-from board import *
-from adafruit_bus_device.spi_device import SPIDevice
+# Initialize UART bus
+uart = busio.UART(board.TX, board.RX, baudrate=9600, bits = 8, parity = None, timeout=0)
+message_started = False
+uplink_message = "Summit checking in.\n"
+uart_char_buffer = bytearray()
+uart_rx_string = bytearray()
 
-#Turn off Pico on startup then turn on when RFM finish init
-"""
-pico_en = digitalio.DigitalInOut(board.D24)
-pico_en.direction = digitalio.Direction.OUTPUT
-pico_en.value = False
-"""
+# set the time interval (seconds) for sending packets
+transmit_interval = 5
+
 # Define radio parameters.
-RADIO_FREQ_MHZ = 915.0  # Frequency of the radio in Mhz. Must match your
+RADIO_FREQ_MHZ = 902.0  # Frequency of the radio in Mhz. Must match your
 # module! Can be a value like 915.0, 433.0, etc.
 
-#These are for RFM9x, not SPI Pico
+# Define pins connected to the chip.
+# set GPIO pins as necessary -- this example is for Raspberry Pi
 CS = digitalio.DigitalInOut(board.D10)
 RESET = digitalio.DigitalInOut(board.D11)
 
-cs = digitalio.DigitalInOut(D25)		#for SPI cs
-cs.direction = digitalio.Direction.OUTPUT
-cs.value = True
-
-# Define the onboard LED
-led = digitalio.DigitalInOut(board.LED)
-led.direction = digitalio.Direction.OUTPUT
-
-#init SPI
-"""
-with busio.SPI(SCK, MOSI, MISO) as spi_bus:
-    cs = digitalio.DigitalInOut(D25)
-    device = SPIDevice(spi_bus, cs)
-"""
-# Initialize SPI bus.y
+# Initialize SPI bus.
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-
 # Initialze RFM radio
-rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
-rfm9x.tx_power = 23 #power: 13dB default, 23dB max
+rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ, agc = True)
 
-#Starting Tri's code
-#allocate memory
-tx_string0 = "This is from UART0 channel\n"
-tx_string1 = "Speaking to UART1\n"
-rx_string0 = ""
-rx_string1 = ""
-tx_data0 = bytearray(1)
-rx_data0 = bytearray(1)
-tx_data1 = bytearray(1)
-rx_data1 = bytearray(1)
-rx_list0 = []
-rx_list1 = []
-rx_0_index = 0
-rx_1_index = 0
-selection = bytearray(1)    #see which UART Pico is receiving and relaying from
-i0 = 0       #index for time message repeated
-i1 = 0
-flag0 = False
-flag1 = False
-unicode_error_occurence = 0
+# rfm9x post-config
+rfm9x.enable_crc = True
+rfm9x.tx_power = 23
+rfm9x.spreading_factor = 9
+rfm9x.coding_rate = 8
+#rfm9x.signal_bandwidth = 7800
+rfm9x.ack_delay = 0.1		# set delay before sending ACK
+rfm9x.node = 1				# set node addresses
+rfm9x.destination = 2
 
-MAX_BUF = 255
+# initialize counter
+counter = 0
+ack_failed_counter = 0
+# initialize flag and timer
+time_now = time.monotonic()
+uart_now=time.monotonic()
+
+# send startup message from my_node
+rfm9x.send_with_ack(bytes("startup message from node {}".format(rfm9x.node), "UTF-8"))
+print("Waiting for packets...")
+
+
 while True:
-    """
-    #RECEIVE SECTION
-    packet = rfm9x.receive()
-    if packet is None:
-        print("Received nothing!")
-    else:
-        print("Received (ASCII): {0}".format(str(packet, "ascii")))
-        print("RSSI: {0} dB, SNR: {1} dB.".format(rfm9x.last_rssi, rfm9x.last_snr))
+    text2send = ""
+    # Look for a new packet: only accept if addresses to my_node
+    packet = rfm9x.receive(with_ack=True, with_header=True)
+    # If no packet was received during the timeout then None is returned.
+    if packet is not None:
+        # Received a packet!
+        # Print out the raw bytes of the packet:
+        print("Received (raw header):", [hex(x) for x in packet[0:4]])
+        print("Received (raw payload): {0}".format(packet[4:]))
+        print("RSSI: {0}, SNR: {1}".format(rfm9x.last_rssi, rfm9x.last_snr))
+        
+    # Break the packet down to each customer
     
-    #This delay is to help rfm push the send before receiving
-    time.sleep(0.04)
-    """
-    while not spi.try_lock():
-        pass
-    try:
-        spi.configure(baudrate=1000000)
-    finally:
-        spi.unlock()
-        
-    try:
-        # Keep putting in new message after tx_string run out
-        if (tx_string0 == ""):
-            tx_string0 = "uart0 repeat time " + str(i0) + "\n"
-            i0 += 1
-        if (i0 >= 10000):
-            i0 = 0
-        if (tx_string1 == ""):
-            tx_string1 = "uart1 repeating time " + str(i1) + "\n"
-            i1 += 1
-        if (i1 >= 10000):
-            i1 = 0
-        
-        # SPI ENCODE: split tx_string into 1 byte chunk
-        if tx_string0:
-            tx_data0 = bytearray(tx_string0[0:1], "utf-8", "ignore")
-            tx_string0 = tx_string0[1:]  # len(tx_string)-1 if sht happened
-        if tx_string1:
-            tx_data1 = bytearray(tx_string1[0:1], "utf-8", "ignore")
-            tx_string1 = tx_string1[1:]  # len(tx_string)-1 if sht happened
 
+    #UART Receive
+    while uart.in_waiting > 0:
+        uart_char_buffer = uart.read(1)
+        uart_rx_string = uart_rx_string + uart_char_buffer
+        #	print(uart_rx_string)
+    if uart_rx_string.decode('utf-8', 'ignore') != '':
+        print("UART RX:", uart_rx_string.decode('utf-8', 'ignore'))
+        text2send = uart_rx_string.decode('utf-8', 'ignore')
+        uart_rx_string = bytearray()
         
-        while not spi.try_lock():
-            pass
-        try:
-            spi.configure(baudrate=1000000)
-            # SPI EXCHANGE: conversation, 1 byte per cs cycle
-            cs.value = False
-            spi.write_readinto(b'#', selection)
-            cs.value = True
-            #print(selection)
-            if (selection == b'`'):			#
-                cs.value = False
-                spi.write_readinto(tx_data0, rx_data0)
-                cs.value = True
-                cs.value = False
-                spi.write_readinto(tx_data1, rx_data1)
-                cs.value = True
-                #print(rx_data0, rx_data1)
-                if rx_data0 != b'\x00':
-                    rx_list0.append(rx_data0.decode('utf-8', 'ignore'))
-                    rx_0_index += 1
-                if rx_data1 != b'\x00':
-                    rx_list1.append(rx_data1.decode('utf-8', 'ignore'))
-                    rx_1_index += 1
-                #print(selection, rx_data0, rx_data1)
-                if rx_data0.decode('utf-8', 'ignore') == '\n':
-                    flag0 = True
-                    rx_string0 = ''.join(rx_list0)
-                    print("Message from UART0: ", rx_string0)
-                    led.value = not led.value
-                    rx_0_index = 0
-                if rx_data1.decode('utf-8', 'ignore') == '\n':
-                    flag1 = True
-                    rx_string1 = ''.join(rx_list1)
-                    print("Message from UART1: ", rx_string1)
-                    led.value = not led.value
-                    rx_1_index = 0
+    # UART Transmit
+    if (time.monotonic() - uart_now > transmit_interval):
+        uart_now = time.monotonic()
+        uart.write(uplink_message)
+        print("Sent '{}' to UART device".format(uplink_message))
 
-                # print(rx_data0, rx_data1)
-        finally:
-            spi.unlock()
-        if flag0 == True:
-            rfm9x.send(bytes("UART0: "+rx_string0, 'utf-8'))
-            rx_list0 = []
-            flag0 = False
-        if flag1 == True:
-            rfm9x.send(bytes("UART1: "+rx_string1, 'utf-8'))
-            rx_list1 = []
-            flag1 = False
-
-    except UnicodeError:
-        print("Unicode Error time ", unicode_error_occurence)
-        unicode_error_occurence += 1
+    # send reading after any packet received
+    if time.monotonic() - time_now > transmit_interval:
+        # reset timeer
+        time_now = time.monotonic()
+        counter += 1
+        print("Sending UART RX to GroundBLV")
+        # send a  mesage to destination_node from my_node
+        if not rfm9x.send_with_ack(
+            bytes("message from SUMMIT2 {}. Message {}".format(counter, text2send), "UTF-8")
+        ):
+            ack_failed_counter += 1
+            print(" No Ack: ", counter, ack_failed_counter)
+        print('-----------')
